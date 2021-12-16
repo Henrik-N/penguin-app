4fb1e152c5514a0bc0c412278a4e277af5bb6f44
Henrik Nilsson <n.henrik.g.nilsson@gmail.com>
2021-12-16 18:41:39 +0100

    Log folder is now created if doesn't exist already

--- src/logger.rs
+++ src/logger.rs
@@ -16,6 +16,13 @@ pub fn init_logger(config: LoggerConfig) -> Result<(), fern::InitError> {
 
     let time = chrono::Local::now().format("%H:%M:%S").to_string();
 
+
+    // create logs folder if not already present
+    let path = std::path::Path::new(&config.output_path);
+    let prefix = path.parent().expect("couldn't get parents");
+    std::fs::create_dir_all(prefix).expect("couldn't create parent dirs");
+
+
     let line_colors = colors.clone().info(Color::Green);
     fern::Dispatch::new()
         .format(move |out, message, record| {
